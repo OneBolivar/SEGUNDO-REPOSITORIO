b29ca4276cd848fb22e94a0e476ab9d873847ee5
One B <jebolivar33@gmail.com>
2026-02-27 14:09:42 -0500

    VERSION 2.2  (no terminada)

--- Cajero.py
+++ Cajero.py
@@ -4,4 +4,7 @@ for i in range (Numero_Operaciones):
     Ejecucion = int(input("Elija una opcion: 1. Consultar saldo|| 2. Retirar dinero || 3. Depositar dinero: "))
     if Ejecucion == 1:
         print("Su saldo actual es de: ", SaldoInicial)
+    elif Ejecucion == 2:
+        Retiro = float(input("Ingrese monto a retirar: "))
+        
     
