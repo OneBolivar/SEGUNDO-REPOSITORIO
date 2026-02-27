d97bfe6a11c52c9c7370ac78af57992cd74961fa
One B <jebolivar33@gmail.com>
2026-02-27 14:03:26 -0500

    VERSION 2: CONSULTAR SALDO

--- Cajero.py
+++ Cajero.py
@@ -1,5 +1,7 @@
 SaldoInicial = float(1000)
 Numero_Operaciones = int(input("¿Cuantas operaciones desea realizar: ?"))
-
-
+for i in range (Numero_Operaciones):
+    Ejecucion = int(input("Elija una opcion: 1. Consultar saldo|| 2. Retirar dinero || 3. Depositar dinero: "))
+    if Ejecucion == 1:
+        print("Su saldo actual es de: ", SaldoInicial)
     
