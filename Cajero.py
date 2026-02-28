750e84eee00fed7175ab3fc2b92b99e3ccf80b16
One Bolivar <jebolivar33@gmail.com>
2026-02-27 22:47:57 -0500

    VERSION 5.1.2 (ARREGLANDO CODIGO)

--- Cajero.py
+++ Cajero.py
@@ -13,13 +13,10 @@ for i in range (Numero_Operaciones):
                 Retiro = float(input("Ingrese monto a retirar: "))
                 if Retiro > 0:
                     Validador = False
-
-
                     if Retiro > SaldoInicial:
                         print("Fondos insuficientes")
                     elif Retiro <= SaldoInicial:
-                        print("Retiro efectuado, su nuevo saldo es de: ", (SaldoInicial - Retiro))
-                    
+                        print("Retiro efectuado, su nuevo saldo es de: ", (SaldoInicial - Retiro))                   
     elif Ejecucion == 3:
                        while Validador:
                         Deposito = float(input("Ingrese monto a depositar: "))    
