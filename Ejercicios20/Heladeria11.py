187c4316de925125f22f18b43de6c8c58b46d1c9
One Bolivar <jebolivarp33@gmail.com>
2026-03-18 15:28:38 -0500

    HACIENDO EJERCICIO 11

---
+++ Ejercicios20/Heladeria11.py
@@ -0,0 +1,14 @@
+Productos = {
+    'cono' : 3000,
+    'vaso' : 4000,
+    'banana split' : 9000
+    
+}
+
+Validador = True
+while Validador:
+    Seguir = input("¿Desea pedir otra cosa?").lower .strip 
+    if Seguir == "si":
+        print("")
+    else:
+        Validador = False
