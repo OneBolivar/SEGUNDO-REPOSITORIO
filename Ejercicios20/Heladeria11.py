05765cf8f6da875cd704affddef4f2a58b0ae072
One Bolivar <jebolivarp33@gmail.com>
2026-03-18 15:55:18 -0500

    HAY QUE TERMINAR EL PUNTO 11

--- Ejercicios20/Heladeria11.py
+++ Ejercicios20/Heladeria11.py
@@ -1,14 +1,22 @@
 Productos = {
     'cono' : 3000,
     'vaso' : 4000,
-    'banana split' : 9000
-    
+    'banana split' : 9000   
 }
-
+print()
+print("Bienvenido a la heladeria Krasel Supra")
+print()
+print("Estos son nuestros productos: ")
+print()
+for i, Producto in enumerate(Productos):
+    print(f"{i+1} : {Producto}")
 Validador = True
 while Validador:
-    Seguir = input("¿Desea pedir otra cosa?").lower .strip 
-    if Seguir == "si":
-        print("")
-    else:
+    print()
+    PedirProducto = int(input("Ingrese su pedido: "))
+    PedirCantidad = int(input("¿Cuantos comprara?: "))
+    TotalPagar = PedirProducto * PedirCantidad  
+    Seguir = input("¿Desea pedir otra cosa?: ").lower() .strip() 
+    if Seguir == "no":
         Validador = False
+    
