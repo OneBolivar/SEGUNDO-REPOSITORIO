88af3a1a5426d11c7db37a302fa44a224b2316c7
One Bolivar <jebolivarp33@gmail.com>
2026-03-19 13:07:48 -0500

    FINALIZADO SIN VALIDACION

--- Ejercicios20/Heladeria11.py
+++ Ejercicios20/Heladeria11.py
@@ -3,6 +3,13 @@ Productos = {
     'vaso' : 4000,
     'banana split' : 9000   
 }
+
+Historial = {
+    "cono" : 0,
+    "vaso" : 0,
+    "banana_split" : 0,
+    "total_pagar" : 0
+}
 print()
 print("Bienvenido a la heladeria Krasel Supra")
 print()
@@ -15,8 +22,21 @@ while Validador:
     print()
     PedirProducto = int(input("Ingrese su pedido: "))
     PedirCantidad = int(input("¿Cuantos comprara?: "))
-    TotalPagar = PedirProducto * PedirCantidad  
+    if PedirProducto == 1 :
+       Historial["cono"] += PedirCantidad 
+       TotalPagar = PedirCantidad * 3000
+       Historial["total_pagar"] += TotalPagar
+    if PedirProducto == 2 :
+       Historial["vaso"] += PedirCantidad 
+       TotalPagar = PedirCantidad * 4000
+       Historial["total_pagar"] += TotalPagar
+    if PedirProducto == 3 :
+       Historial["banana_split"] += PedirCantidad 
+       TotalPagar = PedirCantidad * 9000    
+       Historial["total_pagar"] += TotalPagar
+        
+        
     Seguir = input("¿Desea pedir otra cosa?: ").lower() .strip() 
     if Seguir == "no":
+        print(Historial)
         Validador = False
-    
