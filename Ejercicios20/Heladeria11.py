a93cf8a102e562d9dc43b1e0aa089b496d97e80d
One Bolivar <jebolivarp33@gmail.com>
2026-03-19 14:29:03 -0500

    FINALIZADO SIN VALIDACION

--- Ejercicios20/Heladeria11.py
+++ Ejercicios20/Heladeria11.py
@@ -3,11 +3,13 @@ Productos = {
     'vaso' : 4000,
     'banana split' : 9000   
 }
-
 Historial = {
     "cono" : 0,
     "vaso" : 0,
-    "banana_split" : 0,
+    "banana_split" : 0, 
+}
+Historial_clientes = {
+    "clientes" : 0,
     "total_pagar" : 0
 }
 print()
@@ -21,20 +23,27 @@ Validador = True
 while Validador:
     print()
     PedirProducto = int(input("Ingrese su pedido: "))
-    PedirCantidad = int(input("¿Cuantos comprara?: "))
+    PedirCantidad = int(input("¿Cuantos comprara?: ")) 
+
     if PedirProducto == 1 :
        Historial["cono"] += PedirCantidad 
        TotalPagar = PedirCantidad * 3000
-       Historial["total_pagar"] += TotalPagar
+       Historial_clientes["total_pagar"] += TotalPagar
+       Historial_clientes["clientes"] += 1 
     elif PedirProducto == 2 :
        Historial["vaso"] += PedirCantidad 
        TotalPagar = PedirCantidad * 4000
-       Historial["total_pagar"] += TotalPagar
+       Historial_clientes["total_pagar"] += TotalPagar
+       Historial_clientes["clientes"] += 1 
     elif PedirProducto == 3 :
        Historial["banana_split"] += PedirCantidad 
        TotalPagar = PedirCantidad * 9000    
-       Historial["total_pagar"] += TotalPagar
+       Historial_clientes["total_pagar"] += TotalPagar
+       Historial_clientes["clientes"] += 1 
     Seguir = input("¿Desea pedir otra cosa?: ").lower() .strip() 
+    Max_pedido = max(Historial , key= Historial.get)
     if Seguir == "no":
-        print(Historial)
+        print("El total a pagar es de: ", Historial_clientes['total_pagar'])
+        print("El total de clientes atendidos es de: ", Historial_clientes['clientes'])
+        print("El producto que se vendio mas veces fue:",Max_pedido)
         Validador = False
